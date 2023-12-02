0d56ab4cc22d0096d9dce47b1d8077b3006aec14
romanxteo <142682455+romanxteo@users.noreply.github.com>
2023-12-02 21:12:42 +0700

    Add files via upload

---
+++ thi van dap/style.js
@@ -0,0 +1,14 @@
+const name="roman";
+const age=17;
+
+if (age>18) {
+    console.log("hoc dai hoc");
+}
+else {
+    console.log("hoc cap 3");
+}
+let text;
+text = (age==18)?"hoc dai hoc":"hoc cap 3";
+console.log(text)
+
+document.getElementById('text')
